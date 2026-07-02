098264f090187bd6401c3d6a59af0f34579bf2e2
sandaruruwaneka <sandaruruwaneka21@gmail.com>
2026-07-02 06:41:09 +0000

    fix: correct absolute path in deploy-demo.sh

--- source-code/deploy/deploy-demo.sh
+++ source-code/deploy/deploy-demo.sh
@@ -8,7 +8,7 @@ if [ -z "$BRANCH" ]; then
   exit 1
 fi
 
-cd /tea-collection-management-system
+cd /root/tea-collection-management-system
 git fetch origin
 git checkout "$BRANCH"
 git pull origin "$BRANCH"
