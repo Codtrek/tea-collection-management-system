b8920706d0f24a1f7cc9c0032171f4c1de0312ec
santh723 <g.santhosh.n0702@gmail.com>
2026-07-31 01:27:40 +0530

    renamed component Appcard to Statcard

--- source-code/mobile/src/components/ui/index.ts
+++ source-code/mobile/src/components/ui/index.ts
@@ -7,4 +7,4 @@ export {default as ButtonGroup} from './ButtonGroup';
 export {default as Avatar} from './Avatar';
 export {default as Header} from './Header';
 export {default as SearchBar} from './SearchBar';
-export { default as AppCard } from './AppCard';
+export { default as AppCard } from './StatCard';
